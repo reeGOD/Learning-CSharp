0a99f9f9cd38bbb6a411a45d17acdc333b806776
Gabriel_Rego <gabrielregooffice@gmail.com>
2022-08-23 16:41:50 -0300

    pt - 2 -Criando Variaveis en - 2 - Creating variables

---
+++ Explorando a linguagem/2-CriandoVariaveis/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Criando Variaveis");
+        
+        int idade;
+        idade = 21;
+
+        Console.WriteLine("Minha idade é: " + idade);
+
+        idade = 21 + 22;
+
+        Console.WriteLine("Idade da minha mãe é: " + idade);
+
+        idade = idade - 10;
+
+        Console.WriteLine("Idade da minha irmã é: " + idade);
+
+        idade = idade * 2;
+
+        Console.WriteLine("Idade do meu pai é: " + idade);
+
+        idade = (idade * 2) + 20;
+
+        Console.WriteLine("Idade do meu vô é: " + idade);
+
+        Console.WriteLine("Enter para fechar");
+        Console.ReadLine();
+    }
+}
+
